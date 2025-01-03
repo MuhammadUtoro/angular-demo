39ca2fcdddc90488c265ad454e991d8a622d186a
Muhammad Utoro <muhammad.utoro@svbayer08.de>
2025-01-03 14:23:30 +0100

    fixing routing problem

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,11 +10,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
+    RouterModule,
     CommonModule,
     MatFormFieldModule,
     MatButtonModule,
